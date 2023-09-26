196b7a62ebe0d568f97757efa55ea7504f3d0e0f
Clinton Crewe <119900792+Ccrewe92@users.noreply.github.com>
2023-09-26 17:07:26 -0600

    Update StudentInfo.js

--- app/StudentInfo.js
+++ app/StudentInfo.js
@@ -17,5 +17,5 @@ function CourseSection() {
 }
 
 function MyGitHubRepo() {
-    return <a href="https://github.com/Ccrewe92/crpg306-assignments">GitHub</a>
+    return <a href="https://github.com/Ccrewe92/cprg306-assignments">GitHub</a>
 }
