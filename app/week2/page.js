4bf900157d8b7c41e8bbf76ded54be69cad97560
Clinton Crewe <119900792+Ccrewe92@users.noreply.github.com>
2023-09-28 12:06:54 -0600

    Added week 3

--- app/week2/page.js
+++ app/week2/page.js
@@ -1,10 +1,13 @@
-import Link from "next/link"
-import StudentInfo from "../StudentInfo"
+import Link from "next/link";
+import StudentInfo from "../StudentInfo";
 
 export default function Page() {
     return (
         <main>
-            <h1> WEEK 2</h1>
+            <h1> WEEK 2 </h1>
+            <h2>
+                <Link href="/week3">WEEK 3</Link>
+            </h2>
             <Link href="/">Back To Home</Link>
             <StudentInfo />
         </main>
