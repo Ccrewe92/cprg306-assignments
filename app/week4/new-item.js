baa37a1a92984ef8afe84ed29411a55e74a9cccd
Clinton Crewe <119900792+Ccrewe92@users.noreply.github.com>
2023-10-09 00:41:30 -0600

    more css changes

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -21,20 +21,19 @@ function NewItem() {
     };
 
     return (
-      <div className="p-4 max-w-xl mx-auto border border-gray-400 rounded shadow-md bg-gray-800">
-          <form onSubmit={handleSubmit} className="space-y-4">
-              <div>
-                  <label className="block font-bold mb-2 text-white">Name:</label>
+      <div className="p-4 mx-auto max-w-sm w-full">
+          <form onSubmit={handleSubmit} className="p-2 m-4 bg-slate-900 text-black max-w-sm w-full">
+              <div className="mb-2">
                   <input 
                       type="text" 
+                      placeholder="Item name" 
                       value={name} 
                       onChange={(e) => setName(e.target.value)} 
                       required 
-                      className="w-full border p-2 rounded bg-gray-700 text-white" 
+                      className="w-full mt-1 border-2 border-gray-300 p-2 rounded-lg font-sans" 
                   />
               </div>
-              <div>
-                  <label className="block font-bold mb-2 text-white">Quantity:</label>
+              <div className="flex justify-between">
                   <input 
                       type="number" 
                       min="1" 
@@ -42,22 +41,28 @@ function NewItem() {
                       value={quantity} 
                       onChange={(e) => setQuantity(Number(e.target.value))} 
                       required 
-                      className="w-full border p-2 rounded bg-gray-700 text-white"
+                      className="w-20 ml-1 border-2 border-gray-300 p-2 rounded-lg font-sans"
                   />
-              </div>
-              <div>
-                  <label className="block font-bold mb-2 text-white">Category:</label>
                   <select 
                       value={category} 
                       onChange={(e) => setCategory(e.target.value)} 
-                      className="w-full border p-2 rounded bg-gray-700 text-white"
+                      className="ml-1 border-2 border-gray-300 p-2 rounded-lg font-sans"
                   >
-                      { /* Options remain the same */ }
+                      <option value="" disabled>Category</option>
+                      <option value="produce">Produce</option>
+                      <option value="dairy">Dairy</option>
+                      <option value="bakery">Bakery</option>
+                      <option value="meat">Meat</option>
+                      <option value="frozen foods">Frozen Foods</option>
+                      <option value="canned goods">Canned Goods</option>
+                      <option value="dry goods">Dry Goods</option>
+                      <option value="beverages">Beverages</option>
+                      <option value="snacks">Snacks</option>
+                      <option value="household">Household</option>
+                      <option value="other">Other</option>
                   </select>
               </div>
-              <div>
-                  <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">Submit</button>
-              </div>
+              <button type="submit" className="w-full mt-4 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">+</button>
           </form>
       </div>
   );
