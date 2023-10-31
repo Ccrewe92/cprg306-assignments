afbcbf2c284a65508a7d547685215a3fd4c4cc62
Clinton Crewe <119900792+Ccrewe92@users.noreply.github.com>
2023-10-30 21:39:34 -0600

    Update new-item.js

--- app/week6/new-item.js
+++ app/week6/new-item.js
@@ -10,8 +10,6 @@ function NewItem({ onAddItem }) {
         e.preventDefault();
 
         const item = { name, quantity, category };
-        console.log(item);
-
         onAddItem(item);
 
         setName("");
@@ -56,6 +54,8 @@ function NewItem({ onAddItem }) {
                             <option value="produce">Produce</option>
                             <option value="dairy">Dairy</option>
                             <option value="meat">Meat</option>
+                            <option value="bakery">Bakery</option>
+                            <option value="household">Household</option>
                             {/* Add other categories as needed */}
                         </select>
                     </div>
@@ -71,20 +71,23 @@ function NewItem({ onAddItem }) {
 
             <style jsx>{`
                 .form-container {
-                    background-color: rgba(255, 255, 255, 0.9);
+                    background-color: rgba(20, 20, 20, 0.95);
+                    color: #ffffff;
                     padding: 2rem;
                     border-radius: 8px;
                     width: 300px;
                     margin: 0 auto;
+                    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
                 }
 
                 .input-group {
                     margin-bottom: 1.5rem;
+                    display: flex;
+                    justify-content: space-between;
                 }
 
                 .half {
-                    float: left;
-                    width: 50%;
+                    flex: 0 0 48%;
                     padding: 0 0.5rem;
                 }
 
@@ -92,6 +95,21 @@ function NewItem({ onAddItem }) {
                     display: block;
                     text-align: center;
                     margin-top: 1rem;
+                    color: #FF5733;
+                    text-decoration: underline;
+                }
+
+                button {
+                    background-color: #FF5733;
+                    color: #ffffff;
+                    padding: 10px 15px;
+                    border: none;
+                    border-radius: 5px;
+                    cursor: pointer;
+                }
+
+                button:hover {
+                    background-color: #E94E1B;
                 }
             `}</style>
         </div>
