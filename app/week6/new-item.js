f51c2f10f4131f798190b99f794730c179ac9bf5
Clinton Crewe <119900792+Ccrewe92@users.noreply.github.com>
2023-10-30 21:44:48 -0600

    Update new-item.js

--- app/week6/new-item.js
+++ app/week6/new-item.js
@@ -17,103 +17,67 @@ function NewItem({ onAddItem }) {
         setCategory("produce");
     };
 
-    return (
-        <div className="form-container">
-            <form onSubmit={handleSubmit}>
-                <div className="input-group">
-                    <label htmlFor="name">Item Name:</label>
-                    <input
-                        type="text"
-                        id="name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        placeholder="Enter item name"
-                        required
-                    />
-                </div>
+     return (
+            <div className="form-container">
+                {/* ... [rest of the form code is unchanged] */}
 
-                <div className="input-group">
-                    <div className="half">
-                        <label htmlFor="quantity">Quantity:</label>
-                        <input
-                            type="number"
-                            id="quantity"
-                            value={quantity}
-                            onChange={(e) => setQuantity(e.target.value)}
-                            required
-                        />
-                    </div>
+                <style jsx>{`
+                    .form-container {
+                        background-color: rgba(20, 20, 20, 0.95);
+                        color: #ffffff;
+                        padding: 2rem;
+                        border-radius: 8px;
+                        width: 400px;
+                        margin: 0 auto;
+                        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+                    }
 
-                    <div className="half">
-                        <label htmlFor="category">Category:</label>
-                        <select
-                            id="category"
-                            value={category}
-                            onChange={(e) => setCategory(e.target.value)}
-                        >
-                            <option value="produce">Produce</option>
-                            <option value="dairy">Dairy</option>
-                            <option value="meat">Meat</option>
-                            <option value="bakery">Bakery</option>
-                            <option value="household">Household</option>
-                            {/* Add other categories as needed */}
-                        </select>
-                    </div>
-                </div>
+                    .input-group {
+                        margin-bottom: 1.5rem;
+                        display: flex;
+                        justify-content: space-between;
+                    }
 
-                <div className="input-group">
-                    <button type="submit">Add New Item</button>
-                </div>
-            </form>
-            <Link href="/">
-                <a className="back-link">Back To Home</a>
-            </Link>
+                    .half {
+                        flex: 0 0 48%;
+                        padding: 0 0.5rem;
+                    }
 
-            <style jsx>{`
-                .form-container {
-                    background-color: rgba(20, 20, 20, 0.95);
-                    color: #ffffff;
-                    padding: 2rem;
-                    border-radius: 8px;
-                    width: 400px;
-                    margin: 0 auto;
-                    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-                }
+                    label, input, select {
+                        color: #ffffff;
+                    }
 
-                .input-group {
-                    margin-bottom: 1.5rem;
-                    display: flex;
-                    justify-content: space-between;
-                }
+                    input, select {
+                        background-color: #333;
+                        border: 1px solid #555;
+                        padding: 5px;
+                        border-radius: 4px;
+                        color: #fff;
+                    }
 
-                .half {
-                    flex: 0 0 48%;
-                    padding: 0 0.5rem;
-                }
+                    .back-link {
+                        display: block;
+                        text-align: center;
+                        margin-top: 1rem;
+                        color: #FF5733;
+                        text-decoration: underline;
+                    }
 
-                .back-link {
-                    display: block;
-                    text-align: center;
-                    margin-top: 1rem;
-                    color: #FF5733;
-                    text-decoration: underline;
-                }
+                    button {
+                        background-color: #FF5733;
+                        color: #ffffff;
+                        padding: 10px 15px;
+                        border: none;
+                        border-radius: 5px;
+                        cursor: pointer;
+                    }
 
-                button {
-                    background-color: #FF5733;
-                    color: #ffffff;
-                    padding: 10px 15px;
-                    border: none;
-                    border-radius: 5px;
-                    cursor: pointer;
-                }
-
-                button:hover {
-                    background-color: #E94E1B;
-                }
-            `}</style>
-        </div>
-    );
-}
+                    button:hover {
+                        background-color: #E94E1B;
+                    }
+                `}</style>
+            </div>
+        );
+    }
 
 export default NewItem;
