75fe0b255c79480fb969d20f38c7479b9e307e7e
Clinton Crewe <119900792+Ccrewe92@users.noreply.github.com>
2023-10-30 21:33:05 -0600

    Update new-item.js

--- app/week6/new-item.js
+++ app/week6/new-item.js
@@ -19,82 +19,83 @@ function NewItem({ onAddItem }) {
         setCategory("produce");
     };
 
- return (
-     <div className="form-container">
-         <form onSubmit={handleSubmit}>
-             <div className="input-group">
-                 <label htmlFor="name">Item Name:</label>
-                 <input
-                     type="text"
-                     id="name"
-                     value={name}
-                     onChange={(e) => setName(e.target.value)}
-                     placeholder="Enter item name"
-                     required
-                 />
-             </div>
+    return (
+        <div className="form-container">
+            <form onSubmit={handleSubmit}>
+                <div className="input-group">
+                    <label htmlFor="name">Item Name:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Enter item name"
+                        required
+                    />
+                </div>
 
-             <div className="input-group">
-                 <div className="half">
-                     <label htmlFor="quantity">Quantity:</label>
-                     <input
-                         type="number"
-                         id="quantity"
-                         value={quantity}
-                         onChange={(e) => setQuantity(e.target.value)}
-                         required
-                     />
-                 </div>
+                <div className="input-group">
+                    <div className="half">
+                        <label htmlFor="quantity">Quantity:</label>
+                        <input
+                            type="number"
+                            id="quantity"
+                            value={quantity}
+                            onChange={(e) => setQuantity(e.target.value)}
+                            required
+                        />
+                    </div>
 
-                 <div className="half">
-                     <label htmlFor="category">Category:</label>
-                     <select
-                         id="category"
-                         value={category}
-                         onChange={(e) => setCategory(e.target.value)}
-                     >
-                         <option value="produce">Produce</option>
-                         <option value="dairy">Dairy</option>
-                         <option value="meat">Meat</option>
-                         {/* Add other categories as needed */}
-                     </select>
-                 </div>
-             </div>
+                    <div className="half">
+                        <label htmlFor="category">Category:</label>
+                        <select
+                            id="category"
+                            value={category}
+                            onChange={(e) => setCategory(e.target.value)}
+                        >
+                            <option value="produce">Produce</option>
+                            <option value="dairy">Dairy</option>
+                            <option value="meat">Meat</option>
+                            {/* Add other categories as needed */}
+                        </select>
+                    </div>
+                </div>
 
-             <div className="input-group">
-                 <button type="submit">Add New Item</button>
-             </div>
-         </form>
-         <Link href="/">
-             <a className="back-link">Back To Home</a>
-         </Link>
+                <div className="input-group">
+                    <button type="submit">Add New Item</button>
+                </div>
+            </form>
+            <Link href="/">
+                <a className="back-link">Back To Home</a>
+            </Link>
 
-         <style jsx>{`
-             .form-container {
-                 background-color: rgba(255, 255, 255, 0.9);
-                 padding: 2rem;
-                 border-radius: 8px;
-                 width: 300px;
-                 margin: 0 auto;
-             }
+            <style jsx>{`
+                .form-container {
+                    background-color: rgba(255, 255, 255, 0.9);
+                    padding: 2rem;
+                    border-radius: 8px;
+                    width: 300px;
+                    margin: 0 auto;
+                }
 
-             .input-group {
-                 margin-bottom: 1.5rem;
-             }
+                .input-group {
+                    margin-bottom: 1.5rem;
+                }
 
-             .half {
-                 float: left;
-                 width: 50%;
-                 padding: 0 0.5rem;
-             }
+                .half {
+                    float: left;
+                    width: 50%;
+                    padding: 0 0.5rem;
+                }
 
-             .back-link {
-                 display: block;
-                 text-align: center;
-                 margin-top: 1rem;
-             }
-         `}</style>
-     </div>
- );
+                .back-link {
+                    display: block;
+                    text-align: center;
+                    margin-top: 1rem;
+                }
+            `}</style>
+        </div>
+    );
+}
 
 export default NewItem;
