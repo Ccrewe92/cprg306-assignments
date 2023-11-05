840b25ba7c22b0eb20ca4a5e36e792d1b9589966
Clinton Crewe <119900792+Ccrewe92@users.noreply.github.com>
2023-11-04 22:01:31 -0600

    Update page.js

--- app/week7/page.js
+++ app/week7/page.js
@@ -14,9 +14,15 @@ const Page = () => {
     };
 
     const handleItemSelect = (item) => {
-        // Clean up the item name by removing size and emoji
-        const cleanedName = item.name.split(',')[0].trim().replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|�[�-�]|�[�-�]|[\u2011-\u26FF]|�[�-�])/g, '');
-        setSelectedItemName(cleanedName);
+      // regex to match most emojis
+      const emojiRegex = /([\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F700}-\u{1F77F}]|[\u{1F780}-\u{1F7FF}]|[\u{1F800}-\u{1F8FF}]|[\u{1F900}-\u{1F9FF}]|[\u{1FA00}-\u{1FA6F}]|[\u{1FA70}-\u{1FAFF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]|[\u{1F1E0}-\u{1F1FF}]|[\u{1F201}-\u{1F2FF}]|[\u{1F300}-\u{1F5FF}]|[\u{1F600}-\u{1F64F}]|[\u{1F680}-\u{1F6FF}]|[\u{1F700}-\u{1F77F}]|[\u{1F780}-\u{1F7FF}]|[\u{1F800}-\u{1F8FF}]|[\u{1F900}-\u{1F9FF}]|[\u{1FA00}-\u{1FA6F}]|[\u{1FA70}-\u{1FAFF}]|[\u{FE00}-\u{FE0F}]|[\u{1F000}-\u{1F02F}]|[\u{1F0A0}-\u{1F0FF}]|[\u{1F100}-\u{1F64F}]|[\u{1F680}-\u{1F6FF}]|[\u{1F700}-\u{1F77F}]|[\u{1F780}-\u{1F7FF}]|[\u{1F800}-\u{1F8FF}]|[\u{1F900}-\u{1F9FF}]|[\u{1FA00}-\u{1FA6F}]|[\u{1FA70}-\u{1FAFF}]|[\u{E000}-\u{F8FF}]|[\u{2700}-\u{27BF}]|[\u{24C2}-\u{1F251}])/gu;
+
+      // Remove the emoji using the regex
+      const cleanedName = item.name.split(',')[0].trim().replace(emojiRegex, '');
+
+      // Use encodeURIComponent to ensure the string is URL-safe
+      const encodedName = encodeURIComponent(cleanedName);
+      setSelectedItemName(encodedName);
     };
 
     return (
