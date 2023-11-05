a376d08849837f97403dd6dbc8bd87e17194e576
Clinton Crewe <119900792+Ccrewe92@users.noreply.github.com>
2023-11-04 21:20:15 -0600

    Update item-list.js

--- app/week7/item-list.js
+++ app/week7/item-list.js
@@ -2,60 +2,114 @@ import React, { useState } from 'react';
 import Item from './item';
 import MealIdeas from './meal-ideas';
 
-const ItemList = ({ items, onItemSelect }) => {
+const ItemList = ({ items }) => {
     const [sortBy, setSortBy] = useState('name');
     const [selectedIngredient, setSelectedIngredient] = useState(null);
 
-    // Sort the items whenever sortBy changes
-    const sortedItems = [...items].sort((a, b) => {
-        if (sortBy === 'name') {
-            return a.name.localeCompare(b.name);
-        } else if (sortBy === 'category') {
-            return a.category.localeCompare(b.category);
-        }
-        return 0;
-    });
+    // Sorting logic
+    let sortedItems = [...items];
+    if (sortBy === 'name') {
+        sortedItems.sort((a, b) => a.name.localeCompare(b.name));
+    } else if (sortBy === 'category') {
+        sortedItems.sort((a, b) => a.category.localeCompare(b.category));
+    }
 
-    // Event handlers for sorting
+    // Event handlers
     const handleSortByName = () => setSortBy('name');
     const handleSortByCategory = () => setSortBy('category');
-    const handleItemSelect = (item) => {
-        setSelectedIngredient(item.name);
-        if (onItemSelect) {
-            onItemSelect(item);
-        }
-    };
+    const handleGroupByCategory = () => setSortBy('groupedCategory');
+    const handleItemSelect = (name) => setSelectedIngredient(name);
+
+    // Group by category logic
+    let groupedItems = {};
+    if (sortBy === 'groupedCategory') {
+        groupedItems = sortedItems.reduce((acc, item) => {
+            if (!acc[item.category]) acc[item.category] = [];
+            acc[item.category].push(item);
+            return acc;
+        }, {});
+    }
+
+    // Sorted categories for grouped view
+    const sortedCategories = Object.keys(groupedItems).sort();
 
     // Styling for buttons
-    const getButtonClassName = (type) => `btn ${sortBy === type ? 'btn-active' : ''}`;
+    const buttonStyles = `
+        .btn {
+            padding: 8px 16px;
+            background-color: white;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            color: #333;
+            cursor: pointer;
+            transition: background-color 0.3s, color 0.3s;
+            margin-right: 10px; /* Adds spacing between buttons */
+        }
+
+        .btn:hover {
+            background-color: lightblue;
+            color: #fff;
+        }
+
+        .btn-active {
+            background-color: lightblue;
+            color: #fff;
+        }
+    `;
 
     return (
         <div>
+            <style>{buttonStyles}</style>
             <div>
                 <button
-                    className={getButtonClassName('name')}
+                    className={`btn ${sortBy === 'name' ? 'btn-active' : ''}`}
                     onClick={handleSortByName}
                 >
                     Sort by Name
                 </button>
                 <button
-                    className={getButtonClassName('category')}
+                    className={`btn ${sortBy === 'category' ? 'btn-active' : ''}`}
                     onClick={handleSortByCategory}
                 >
                     Sort by Category
                 </button>
+                <button
+                    className={`btn ${sortBy === 'groupedCategory' ? 'btn-active' : ''}`}
+                    onClick={handleGroupByCategory}
+                >
+                    Group by Category
+                </button>
             </div>
-            <ul className="space-y-4">
-                {sortedItems.map((item) => (
-                    <Item
-                        key={item.id}
-                        name={item.name}
-                        quantity={item.quantity}
-                        category={item.category}
-                        onSelect={() => handleItemSelect(item)}
-                    />
-                ))}
-            </ul>
+            {sortBy === 'groupedCategory' ? (
+                sortedCategories.map((category) => (
+                    <div key={category}>
+                        <h2>{category}</h2>
+                        <ul className="space-y-4">
+                            {groupedItems[category].map((item) => (
+                                <Item
+                                    key={item.id}
+                                    name={item.name}
+                                    quantity={item.quantity}
+                                    category={item.category}
+                                    onSelect={() => handleItemSelect(item.name)}
+                                />
+                            ))}
+                        </ul>
+                    </div>
+                ))
+            ) : (
+                <ul className="space-y-4">
+                    {sortedItems.map((item) => (
+                        <Item
+                            key={item.id}
+                            name={item.name}
+                            quantity={item.quantity}
+                            category={item.category}
+                            onSelect={() => handleItemSelect(item.name)}
+                        />
+                    ))}
+                </ul>
+            )}
             {/* Pass the selectedIngredient to the MealIdeas component */}
             {selectedIngredient && <MealIdeas ingredient={selectedIngredient} />}
         </div>
