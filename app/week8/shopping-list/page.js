7399fa6b2b18af55c420c9cab0d2bd0a773bc390
Clinton Crewe <119900792+Ccrewe92@users.noreply.github.com>
2023-11-05 17:46:19 -0700

    Update page.js

--- app/week8/shopping-list/page.js
+++ app/week8/shopping-list/page.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router'; // Import useRouter
 import { useUserAuth } from '../_utils/auth-context'; // Update the import path based on your project structure
