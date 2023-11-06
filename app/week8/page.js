db7217bb586273a5c1dd0b2629aa818002f4b2a9
Clinton Crewe <119900792+Ccrewe92@users.noreply.github.com>
2023-11-05 17:44:37 -0700

    auth

---
+++ app/week8/page.js
@@ -0,0 +1,56 @@
+"use client";
+import React from 'react';
+import { useUserAuth } from './_utils/auth-context';
+
+const LandingPage = () => {
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+  const handleLogin = async () => {
+    await gitHubSignIn();
+  };
+
+  const handleLogout = async () => {
+    await firebaseSignOut();
+  };
+
+  return (
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+          Flowbite
+        </a>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            {!user ? (
+              <>
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Sign in to your account
+                </h1>
+                <button
+                  onClick={handleLogin}
+                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Sign in with GitHub
+                </button>
+              </>
+            ) : (
+              <>
+                <p>Welcome, {user.displayName} ({user.email})</p>
+                <button
+                  onClick={handleLogout}
+                  className="w-full text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+                >
+                  Logout
+                </button>
+                {/* Add a link to the shopping list page here */}
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LandingPage;
--- app/week8/shopping-list/page.js
+++ app/week8/shopping-list/page.js
@@ -1,5 +1,6 @@
-"use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router'; // Import useRouter
+import { useUserAuth } from '../_utils/auth-context'; // Update the import path based on your project structure
 import NewItem from './new-item';
 import ItemList from './item-list';
 import MealIdeas from './meal-ideas';
@@ -8,25 +9,27 @@ import itemsData from './items.json';
 const Page = () => {
     const [items, setItems] = useState(itemsData);
     const [selectedItemName, setSelectedItemName] = useState('');
+    const { user } = useUserAuth(); // Using the custom hook to get the user
+    const router = useRouter(); // Hook to control routing
+
+    // Redirect if not logged in
+    useEffect(() => {
+        // If there is no user, redirect to the login page
+        if (!user) {
+            router.push('/week8/page'); // Adjust this to the route of your login page
+        }
+    }, [user, router]);
 
     const handleAddItem = (newItem) => {
-        setItems(prevItems => [...prevItems, newItem]);
+        setItems((prevItems) => [...prevItems, newItem]);
     };
 
     const handleItemSelect = (item) => {
-      // This regex should match most emojis
-      const emojiRegex = /([\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F700}-\u{1F77F}]|[\u{1F780}-\u{1F7FF}]|[\u{1F800}-\u{1F8FF}]|[\u{1F900}-\u{1F9FF}]|[\u{1FA00}-\u{1FA6F}]|[\u{1FA70}-\u{1FAFF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]|[\u{1F1E0}-\u{1F1FF}]|[\u{1F201}-\u{1F2FF}]|[\u{1F300}-\u{1F5FF}]|[\u{1F600}-\u{1F64F}]|[\u{1F680}-\u{1F6FF}]|[\u{1F700}-\u{1F77F}]|[\u{1F780}-\u{1F7FF}]|[\u{1F800}-\u{1F8FF}]|[\u{1F900}-\u{1F9FF}]|[\u{1FA00}-\u{1FA6F}]|[\u{1FA70}-\u{1FAFF}]|[\u{FE00}-\u{FE0F}]|[\u{1F000}-\u{1F02F}]|[\u{1F0A0}-\u{1F0FF}]|[\u{1F100}-\u{1F64F}]|[\u{1F680}-\u{1F6FF}]|[\u{1F700}-\u{1F77F}]|[\u{1F780}-\u{1F7FF}]|[\u{1F800}-\u{1F8FF}]|[\u{1F900}-\u{1F9FF}]|[\u{1FA00}-\u{1FA6F}]|[\u{1FA70}-\u{1FAFF}]|[\u{E000}-\u{F8FF}]|[\u{2700}-\u{27BF}]|[\u{24C2}-\u{1F251}])/gu;
-
-      // Remove the emoji using the regex
-      const cleanedName = item.name.split(',')[0].trim().replace(emojiRegex, '');
-
-      // Set the display name without encoding
-      setSelectedItemName(cleanedName);
-
-      // When making the API call, use encodeURIComponent
-      const encodedName = encodeURIComponent(cleanedName);
-      fetchMealIdeas(encodedName); // Ensure you pass the encoded name to the API call
+        // Existing code for handleItemSelect
     };
+
+    // If the user is not logged in, return null or a loader
+    if (!user) return null;
 
     return (
         <main className="min-h-screen bg-gray-900 p-8">
